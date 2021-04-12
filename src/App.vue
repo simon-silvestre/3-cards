21f2f6695d8246e9b8375047dc8fc9d0381339b9
Simon <silvestresimon@icloud.com>
2021-04-12 16:36:32 +0200

    card title & paragraph

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="cardContainer">
+      <Card title="SEDANS" paragraph="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip." color="hsl(31, 77%, 52%)"/>
+      <Card title="SUVS" paragraph="SUVs Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures." color="hsl(184, 100%, 22%)" />
+      <Card title="LUXURY" paragraph="Luxury Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style." color="hsl(179, 100%, 13%)" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import { gsap } from 'gsap'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    font-family: 'Lexend Deca', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 15px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: hsl(0, 0%, 95%);
+    color: hsla(0, 0%, 100%, 0.75);
+  }
+  .cardContainer {
+    display: flex;
+    width: 900px;
+    height: 500px;
+    border-radius: 10px;
+  }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="Card" :style="{ background: color }">
+    <div v-if="title == 'SEDANS'" class="image">
+      <img src="@/assets/icon-sedans.svg" alt="logo voiture">
+    </div>
+    <div v-else-if="title == 'SUVS'" class="image">
+      <img src="@/assets/icon-suvs.svg" alt="logo voiture">
+    </div>
+    <div v-else class="image">
+      <img src="@/assets/icon-luxury.svg" alt="logo voiture">
+    </div>
+    <h1>{{ title }}</h1>
+    <p>{{ paragraph }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: String,
+    paragraph: String,
+    color: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .Card {
+    width: 300px;
+    height: 100%;
+    padding: 45px 60px 45px 45px;
+    z-index: 2;
+  }
+  h1 {
+    font-family: 'Big Shoulders Display', cursive;
+    font-weight: 700;
+    color: #fff;
+    margin: 35px 0;
+    font-size: 40px;
+  }
+  p {
+    line-height: 25px;
+  }
+</style>
